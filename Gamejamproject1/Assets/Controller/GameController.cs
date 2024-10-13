bf7ad660fc183f9551a8fc4853814ec14049c5be
WinSama <Nithirat7671@gmail.com>
2024-10-13 22:28:31 +0700

    Begin

---
+++ Gamejamproject1/Assets/Controller/GameController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public StoryScene currentScene;
+    public BottomBarController bottombar;
+    void Start()
+    {
+        bottombar.PlayScene(currentScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+
+        {
+            if (bottombar.IsCompleted())
+            {
+                if (bottombar.IsLastSentence())
+                {
+                    currentScene = currentScene.nextScene;
+                    bottombar.PlayScene(currentScene);
+                }
+                bottombar.PlayNextSentence();
+            }
+        }
+    }
+}
